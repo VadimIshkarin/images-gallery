883d641abbb95e272048534d2412feb79c6c0eb0
VadimIshkarin <theishkarin@gmail.com>
2023-04-26 13:54:55 -0400

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+
 const App = () => {
   return (
     <div>
-      <Header />
+      <Header title="Images Gallery" />
     </div>
   );
 };
